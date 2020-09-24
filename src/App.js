622ee92ceb2fb8be2468be8567c02f9be7ba1f87
Jorge Sanes <jsanes@condorlabs.io>
2020-09-24 10:03:44 -0500

    build container site

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import MicroFrontend from './MicroFrontend';
+
 import './App.css';
+
+const {
+  REACT_APP_STORE_HOST: storeHost,
+  REACT_APP_SHOPPING_CART_HOST: shoppingCartHost,
+} = process.env;
+
+function Header() {
+  return (
+    <div className="banner">
+      <h1 className="banner-title">New Store</h1>
+    </div>
+  );
+}
+
+function Store({ history, ...rest }) {
+  return (
+    <MicroFrontend {...rest} history={history} host={storeHost} name="Store" />
+  );
+}
+
+function ProductDescription({ history, ...rest }) {
+  return (
+    <div>
+      <Header />
+      <div className="home">
+        <div className="content">
+          <div className="store-container product-detail">
+            <MicroFrontend
+              {...rest}
+              history={history}
+              host={storeHost}
+              name="Store"
+            />
+          </div>
+          <div className="shopping-cart-container">
+            <ShoppingCart />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function ShoppingCart({ history, ...rest }) {
+  return (
+    <MicroFrontend
+      {...rest}
+      history={history}
+      host={shoppingCartHost}
+      name="ShoppingCart"
+    />
+  );
+}
+
+function useGlobalState() {
+  const [newItem, setNewItem] = useState({});
+
+  return [newItem, setNewItem];
+}
+
+function Home({ history }) {
+  const [newItem, setNewItem] = useGlobalState();
+  function handleAddItem(item) {
+    setNewItem(item);
+  }
+  return (
+    <div>
+      <Header />
+      <div className="home">
+        <div className="content">
+          <div className="store-container">
+            <Store addItem={handleAddItem} />
+          </div>
+          <div className="shopping-cart-container">
+            <ShoppingCart newItem={newItem} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <React.Fragment>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/product/:name" component={ProductDescription} />
+        </Switch>
+      </React.Fragment>
+    </BrowserRouter>
   );
 }
 
